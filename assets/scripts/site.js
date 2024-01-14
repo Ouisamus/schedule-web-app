763570070e55631a5531ffd6b9b6f49e6834afa9
Louisa <ouisamus@gmail.com>
2024-01-13 21:23:10 -0500

    Added line that shows current time

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -3,21 +3,36 @@ var screen;
 screen = document.querySelector("#screen");
 
 if (screen) {
-   console.log(screen.height);
-   for(let i=1; i<period; i++){
-        let division = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        createLine(division,100/period*i,'#bbb','0.5');
-        screen.appendChild(division);
-    } // for
-    
-} // if (screen)
+    console.log(screen.height);
+    // Draw hour lines
+    for (let i = 1; i < period; i++) {
+        let hourLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        drawLine(hourLine, 100 / period * i, '#bbb', '0.5');
+        screen.appendChild(hourLine);
+    }
+    // Create current time line
+    var nowLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    updateCurrentTime();
+    screen.appendChild(nowLine);
 
+    // Update time line
+    const intervalID = setInterval(updateCurrentTime, 2000);
 
-function createLine(shape,y,color,size){
-    shape.setAttribute('x1','0');
-    shape.setAttribute('x2','100');
-    shape.setAttribute('y1',y);
-    shape.setAttribute('y2',y);
-    shape.setAttribute('stroke',color);
-    shape.setAttribute('stroke-width',size);
+} // close if (screen)
+
+function updateCurrentTime(){
+    let currentTime = new Date(Date.now());
+    currentTime = currentTime.getHours() * 60 + currentTime.getMinutes();
+    if (currentTime >= 480 && currentTime <= 1200) { // between 8am and 8pm?
+        drawLine(nowLine, (100/720)*(currentTime-480), '#f00', '0.5');
+    }
+}
+
+function drawLine(line, y, color, size) {
+    line.setAttribute('x1', '0');
+    line.setAttribute('x2', '100');
+    line.setAttribute('y1', y);
+    line.setAttribute('y2', y);
+    line.setAttribute('stroke', color);
+    line.setAttribute('stroke-width', size);
 }
