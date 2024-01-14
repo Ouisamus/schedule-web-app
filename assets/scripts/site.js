04e83a7cd3178c1ee36bb6fb36eae5f9487d5d5b
Louisa <ouisamus@gmail.com>
2024-01-13 19:47:57 -0500

    Created function for drawing lines

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,17 +1,23 @@
-const period = 5;
+const period = 12;
 var screen;
 screen = document.querySelector("#screen");
 
 if (screen) {
    console.log(screen.height);
    for(let i=1; i<period; i++){
-        var division = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        division.setAttribute('x1','0');
-        division.setAttribute('x2','100');
-        division.setAttribute('y1',100/period*i);
-        division.setAttribute('y2',100/period*i);
-        division.setAttribute('stroke','red');
+        let division = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        createLine(division,100/period*i,'#bbb','0.5');
         screen.appendChild(division);
-    }
+    } // for
+    
 } // if (screen)
 
+
+function createLine(shape,y,color,size){
+    shape.setAttribute('x1','0');
+    shape.setAttribute('x2','100');
+    shape.setAttribute('y1',y);
+    shape.setAttribute('y2',y);
+    shape.setAttribute('stroke',color);
+    shape.setAttribute('stroke-width',size);
+}
