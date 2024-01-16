e50837918308e0edcc9e39235b0e2aa910680b8d
Louisa <ouisamus@gmail.com>
2024-01-16 14:43:59 -0500

    Draws courses based on scraper data

--- assets/scripts/scraper.js
+++ assets/scripts/scraper.js
@@ -1,57 +1,57 @@
 const dayNums = new Map([['M', 0], ['Tu', 1], ['W', 2], ['Th', 3], ['F', 4]]);
 
 function getCourseObj(course, section, semester) { // return type Course object or return false
-    let courseURL = `https://app.testudo.umd.edu/soc/${semester}/sections?courseIds=${course}`;
+    return new Promise((resolve, reject) => {
+        let courseURL = `https://app.testudo.umd.edu/soc/${semester}/sections?courseIds=${course}`;
 
-    // Retrieve URL data using axios
-    axios
-        .get(courseURL)
-        .then((response) => {
-            let courseObj = new Course(course); // Creates course object
-            response.data = response.data.replaceAll(/<img src=.*?>/g, ""); // removes all <img> tags to prevent missing file errors
-            const coursePage = document.createElement('coursePage');
-            coursePage.innerHTML = response.data.trim();
-            let sectionInfo = coursePage.querySelector(`input[value="${section}"]`).parentNode // finds specific section from list
-            let sectionTimes = sectionInfo
-                .querySelector('.class-days-container')
-                .querySelectorAll('.section-day-time-group');
-            sectionTimes.forEach((row) => {
-                if (row.querySelector('.section-days')) { // "See details on ELMS" and similar msgs are ignored
+        // Retrieve URL data using axios
+        axios
+            .get(courseURL)
+            .then((response) => {
+                let courseObj = new Course(`${course}-${section}`); // Creates course object
+                response.data = response.data.replaceAll(/<img src=.*?>/g, ""); // removes all <img> tags to prevent missing file errors
+                const coursePage = document.createElement('coursePage');
+                coursePage.innerHTML = response.data.trim();
+                if (coursePage.querySelector(`input[value="${section}"]`)) {
+                    let sectionInfo = coursePage.querySelector(`input[value="${section}"]`).parentNode // finds specific section from list
+                    let sectionTimes = sectionInfo
+                        .querySelector('.class-days-container')
+                        .querySelectorAll('.section-day-time-group');
+                    sectionTimes.forEach((row) => {
+                        if (row.querySelector('.section-days')) { // "See details on ELMS" and similar msgs are ignored
 
-                    let dayString = row.querySelector('.section-days').innerText;
-                    let startTime = row.querySelector('.class-start-time').innerText;
-                    startTime = convertClockTimeToMins(startTime);
-                    let endTime = row.querySelector('.class-end-time').innerText;
-                    endTime = convertClockTimeToMins(endTime);
+                            let dayString = row.querySelector('.section-days').innerText;
+                            let startTime = row.querySelector('.class-start-time').innerText;
+                            startTime = convertClockTimeToMins(startTime);
+                            let endTime = row.querySelector('.class-end-time').innerText;
+                            endTime = convertClockTimeToMins(endTime);
 
-                    let days = new Array();
-                    for (const char of dayString) {
-                        if (char == char.toUpperCase()) {
-                            days.push(char);
-                        } else {
-                            days[days.length - 1] += char;
-                        }
-                    }
-                    for (const day of days) {
-                        let m = new Meeting(startTime, endTime, dayNums.get(day));
-                        courseObj.meetings.push(m);
-                    }
+                            let days = new Array();
+                            for (const char of dayString) {
+                                if (char == char.toUpperCase()) {
+                                    days.push(char);
+                                } else {
+                                    days[days.length - 1] += char;
+                                }
+                            }
+                            for (const day of days) {
+                                let m = new Meeting(startTime, endTime, dayNums.get(day));
+                                courseObj.meetings.push(m);
+                            }
 
-                } // end if row has days
-            }); // end for each row
-            console.log(courseObj);
-            return courseObj;
-        })
-        .catch((error) => {
-            console.error(error);
-            return false;
-        });
+                        } // end if row has days
+                    }); // end for each row
+                    resolve(courseObj);
+                } else {
+                    reject('Course not found');
+                }
+            })
+            .catch((error) => {
+                console.log('error :(');
+                reject(error);
+            });
+    });
 }
-
-let m1 = getCourseObj('ENES220', '0103', '202401')
-console.log(m1);
-// let m2 = getCourseObj('MATH007', '1201', '202401');
-
 function convertClockTimeToMins(clockTime) { // converts clock time to minutes, 8:00am -> 480
     let arr = [...clockTime.matchAll(/(\d{1,2}):(\d{2})([ap]m)/gm)][0]; // arr[1] is hour, arr[2] is minute, arr[3] is am/pm
     arr[1] = Number.parseInt(arr[1]);
@@ -59,7 +59,7 @@ function convertClockTimeToMins(clockTime) { // converts clock time to minutes, 
     let hour, ampm = arr[3];
 
     if (ampm == 'pm') {
-        hour = arr[1] + 12;
+        hour = arr[1] == 12 ? 12 : arr[1] + 12;
     } else {
         hour = arr[1] == 12 ? 0 : arr[1];
     }
--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -1,6 +1,7 @@
 // Draw header
 const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 const header = document.querySelector('#header');
+const SEMESTER = '202401';
 let weekday = (new Date(Date.now())).getDay() - 1;
 
 if (header) {
@@ -46,37 +47,30 @@ if (schedule) {
     // Establishing people and classes
     let people = new Array();
 
+    // Loading & drawing courses
+    const courses = document.querySelector("#courses");
+
     // Ryan
     let ryan = new Person('Ryan', '#9b59b6');
-    let course1 = new Course('ENGL101-1002');
-    course1.meetings.push(new Meeting(480, 555, 1), new Meeting(480, 555, 3));
-    let course2 = new Course('THET285-0102');
-    course2.meetings.push(new Meeting(720, 830, 0), new Meeting(720, 830, 2));
-    let course3 = new Course('ECON230-0101');
-    course3.meetings.push(new Meeting(840, 915, 0), new Meeting(840, 915, 2));
-    let course4 = new Course('ECON306-0202');
-    course4.meetings.push(new Meeting(930, 1005, 0), new Meeting(930, 1005, 2), new Meeting(720, 770, 4));
-    let course5 = new Course('PLCY213-0201');
-    course5.meetings.push(new Meeting(930, 1005, 1), new Meeting(900, 1005, 3))
-    ryan.courses.push(course1, course2, course3, course4, course5);
+    const ryanArr = [['ENGL101', '1002'], ['THET285', '0102'], ['ECON230', '0101'], ['ECON306', '0202'], ['PLCY213', '0201']];
+    for (const entry of ryanArr){
+        const c = getCourseObj(entry[0], entry[1], SEMESTER).then((c) => {
+            ryan.courses.push(c);
+            drawOneCourse(courses, c, 0, 2, ryan.color);
+        });
+    }
     people.push(ryan);
 
-    // Keys
+    // // Keys
     let keys = new Person('Keys', '#388fc7');
-    course1 = new Course('CMSC132-0202');
-    course1.meetings.push(new Meeting(540, 590, 0), new Meeting(540, 590, 2), new Meeting(780, 830, 0), new Meeting(780, 830, 2), new Meeting(780, 830, 4));
-    course2 = new Course('BSCI103-1107');
-    course2.meetings.push(new Meeting(480, 650, 1), new Meeting(750, 825, 1), new Meeting(750, 825, 3));
-    course3 = new Course('MATH141-0523');
-    course3.meetings.push(new Meeting(840, 890, 0), new Meeting(840, 920, 1), new Meeting(840, 890, 2), new Meeting(840, 920, 3), new Meeting(840, 890, 4));
-    course4 = new Course('COMM107-9920');
-    course4.meetings.push(new Meeting(1020, 1095, 1), new Meeting(1020, 1095, 3));
-    keys.courses.push(course1, course2, course3, course4, course5);
+    const keysArr = [['CMSC132', '0202'], ['BSCI103', '1107'], ['MATH141', '0523'], ['COMM107', '9920']];
+    for (const entry of keysArr){
+        const c = getCourseObj(entry[0], entry[1], SEMESTER).then((c) => {
+            keys.courses.push(c);
+            drawOneCourse(courses, c, 1, 2, keys.color);
+        });
+    }
     people.push(keys);
-
-    // Draw courses
-    const courses = document.querySelector("#courses");
-    drawCourses(courses, people, people.length);
 
     // Button event listeners
     document.getElementById('previousDayButton').addEventListener("click", function () { previousDay(courses, people) });
@@ -158,16 +152,20 @@ function drawCourses(courses, people, cols) {
         // Draw courses
         people.forEach((person, i) => {
             person.courses.forEach((course) => {
-                course.meetings.forEach((meeting) => {
-                    if (weekday == meeting.weekday) {
-                        let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-                        drawMeeting(rect, i, cols, meeting.start, meeting.end, person.color);
-                        courses.appendChild(rect);
-                    }
-                });
+                drawOneCourse(courses, course, i, cols, person.color);
             });
         });
     }
+}
+
+function drawOneCourse(courses, courseObj, col, cols, color){
+    courseObj.meetings.forEach((meeting) => {
+        if (weekday == meeting.weekday) {
+            let rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            drawMeeting(rect, col, cols, meeting.start, meeting.end, color);
+            courses.appendChild(rect);
+        }
+    });
 }
 
 function previousDay(courses, people) {
